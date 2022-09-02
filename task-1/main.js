1ea1c6cde61d1efcc9dd8a9ab17d819fe0298b27
Selim Mammadov <mamedovfhn@gmail.com>
2022-09-02 10:28:43 +0400

    task-1

--- task-1/main.js
+++ task-1/main.js
@@ -3,7 +3,24 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+
+    let n = result.input.split(',')
+    let sum = 0;
+    let n_length = n.length;
+
+    for (let i = 0; i < n.length; i++) {
+        sum += parseInt(n[i])
+    }
+    let sum_division = parseInt(sum / n_length);
+
+    let sum_second = 0;
+    let count = 0;
+    for (let i = 0; i < n.length; i++) {
+        const element = parseInt(n[i])
+        if(element > sum_division) {
+            sum_second += element
+            count++
+        }
+    }
+    console.log(`${sum_second}\n${count}`)
 });
